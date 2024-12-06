4b244330cfcfe40c37a25f2daec4c4d8e863765f
saitejareddy2517 <sai.teja@metageeks.tech>
2024-12-06 15:00:02 +0530

    new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  KimaTransactionWidget,
+  KimaProvider,
+  FontSizeOptions,
+  ModeOptions,
+  ColorModeOptions
+} from '@kimafinance/kima-transaction-widget'
+import '@kimafinance/kima-transaction-widget/dist/index.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='pt-0 pl-6 mt-0 flex justify-center items-start'>
+    <KimaProvider>
+      <KimaTransactionWidget
+        theme={{
+          colorMode: ColorModeOptions.dark,
+          fontSize: FontSizeOptions.medium
+        }}
+        mode={ModeOptions.bridge}
+        kimaBackendUrl='http://localhost:3001'
+        kimaNodeProviderQuery='https://api-testnet.kima.finance'
+        kimaGraphqlProviderQuery=''
+        compliantOption={false}
+        errorHandler={(e: any) => {
+          console.log('error:', e)
+        }}
+        successHandler={() => {
+          console.log('success')
+        }}
+        closeHandler={() => {
+          console.log('closed')
+        }}
+      />
+    </KimaProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
